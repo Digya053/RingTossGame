0d0243fd3b1aba5b47b015a4b1679e92725b243f
Digya053 <digyaacharyaa@gmail.com>
2021-10-15 03:11:31 -0500

    Refactor code

--- ringtossgame.cpp
+++ ringtossgame.cpp
@@ -12,10 +12,9 @@ using namespace std;
 static int view_state; // Flag to keep track of the type of view volume currently in display 
 						// [Ortho view = 1, Perspective = 0]
 
-static int success_count;
+static int play_count;
 static int animationPeriod; // Time interval between frames
 static int isAnimate;
-
 
 static float t; // Time parameter.
 static int h; // Horizontal component of initial velocity.
@@ -23,9 +22,6 @@ static float v; // Vertical component of initial velocity.
 static float g;  // Gravitational accelaration.
 string h_string;
 static int score_val;
-
-static float x_t;
-static float y_t;
 
 static float x_pos;
 static float y_pos;
@@ -36,6 +32,9 @@ static int near_win;
 static float eyeY;
 static float centerY;
 static float centerZ;
+static float eyeX = 0;
+static float eyeZ = 0;
+static float centerX = 0;
 
 char canvas_Name[] = "Ring Toss"; // Name at the top of canvas
 char message[] = "Enter Toss Velocity and then GO to Toss";
@@ -48,15 +47,8 @@ char encouragement_msg[] = "VERY NEAR MISS! YOU CAN DO IT!!!";
 
 
 static char theStringBuffer[10];
-static char scoreBuffer[3];
-
-static float square_color[3] = { 1.0, 0.0, 0.0 }; // Color of the square
-static float eyeX = 0;
-static float eyeZ = 0;
-static float centerX = 0;
 
 void animate(int value);
-
 
 // Sets width and height of canvas to 480 by 480.
 #define canvas_Width 550
@@ -64,7 +56,7 @@ void animate(int value);
 
 void init() {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
-	success_count = 0;
+	play_count = 0;
 	animationPeriod = 50;
 	t = 0.0;
 	h = 0;
@@ -85,34 +77,6 @@ void init() {
 
 }
 
-void view_setup(void) {
-	/*
-	This function sets up the view volume.
-	*/
-
-	// Set the current matrix mode to projection
-	glMatrixMode(GL_PROJECTION);
-	// Load identity matrix
-	glLoadIdentity();
-	// Sets view volume to perspective if view state is 0. The aspect ratio of perspective projection
-	//is computed as 116.
-	if (view_state == 0)
-	{
-		gluPerspective(159.4, canvas_Width / canvas_Height, 1.0, 150.0);
-		//gluLookAt(0, eyeY, 0, centerY, 0, -50, 0, 1, 0);
-	}
-	// Sets view volume to orthographic if view state is 1. 
-	else {
-		glOrtho(-275, 275, -275, 275, 1, 150);
-	//	gluLookAt(0, 0, 0, 0, -275, -50, 0, 1, 0);
-		//gluLookAt(0, 200, 0, 0, 255, -50, 0, 1, 0);
-	}
-	// Sets the current matrix mode to modelview to apply the subsequent operations on modelview mode.
-	//gluLookAt(0,eyeY,0,0,0,-50,0,1,0);
-	gluLookAt(eyeX, eyeY, 1, centerX, centerY, -50.0, 0.0, 1.0, 0.0);
-	glMatrixMode(GL_MODELVIEW);
-}
-
 // Routine to draw a bitmap character string.
 void writeBitmapString(void *font, char *string) {
 	char *c;
@@ -126,6 +90,12 @@ void writeStrokeString(void *font, char *string) {
 	for (c = string; *c != '\0'; c++) {
 		glutStrokeCharacter(font, *c);
 	}
+}
+
+// Routine to onvert floating point to char string.
+void integerToString(char * destStr, int precision, int val) {
+	sprintf(destStr, "%d", val);
+	destStr[precision] = '\0';
 }
 
 void draw_single_post_stack(float x, float y, float z, GLdouble cube_size) {
@@ -143,58 +113,24 @@ void draw_post(float x, float y, float z, GLdouble cube_size) {
 	draw_single_post_stack(x, y + 30, z, cube_size);
 }
 
-void display_game_over(float x, float y, float z) {
-	glPushMatrix();
-	glRasterPos3f(x, y, z);
-	writeBitmapString(GLUT_BITMAP_HELVETICA_12, game_over);
-	glPopMatrix();
-}
-
 void draw_ring(float x, float y, float z, GLdouble inner_radius, GLdouble torus_width) {
-	//if (x == 190 and y == -270) {
-		//win = 1;
-	//}
 	GLdouble outer_radius = inner_radius + (torus_width / 2.0);
 	GLint nsides = 25;
 	GLint rings = 25;
-	if (success_count == 1) {
+	if (play_count == 1) {
 		glColor3f(1.0, 0.647, 0.0);
 	}
-	else if (success_count == 2) {
+	else if (play_count == 2) {
 		glColor3f(0.0, 1.0, 1.0);
+	}
+	else {
+		glColor3f(1.0, 1.0, 0.0);
 	}
 
 	glPushMatrix();
 	glTranslatef(x, y, z);
 	glRotatef(90, 1, 0, 0);
 	glutWireTorus(7.5, 27.5, nsides, rings);
-	glPopMatrix();
-
-	if (x >= 210 and x <= 220) {
-		cout << "won:" << win << endl;
-		win = 1;
-	}
-	cout << "win is : " << win << endl;
-	if (win == 0) {
-		cout << "winder uder: " << win << endl;
-		if (x >= 140 and x <= 300 and win == 0) {
-			near_win = 1;
-		}
-	}
-	
-}
-
-// Routine to onvert floating point to char string.
-
-void integerToString(char * destStr, int precision, int val) {
-	sprintf(destStr, "%d", val);
-	destStr[precision] = '\0';
-}
-
-void print_message(float x, float y, float z) {
-	glPushMatrix();
-	glRasterPos3f(x, y, z);
-	writeBitmapString(GLUT_BITMAP_HELVETICA_12, message);
 	glPopMatrix();
 }
 
@@ -246,6 +182,21 @@ void display_score(float x, float y, float z) {
 	glPopMatrix();
 }
 
+void display_game_over(float x, float y, float z) {
+	glColor3f(1.0, 0.0, 0.0);
+	glPushMatrix();
+	glRasterPos3f(x, y, z);
+	writeBitmapString(GLUT_BITMAP_HELVETICA_12, game_over);
+	glPopMatrix();
+}
+
+void print_game_instruction(float x, float y, float z) {
+	glPushMatrix();
+	glRasterPos3f(x, y, z);
+	writeBitmapString(GLUT_BITMAP_HELVETICA_12, message);
+	glPopMatrix();
+}
+
 void display_success_msg(float x, float y, float z) {
 	glColor3f(1.0, 1.0, 1.0);
 	glPushMatrix();
@@ -262,6 +213,36 @@ void display_encouragement_msg(float x, float y, float z) {
 	glPopMatrix();
 }
 
+void check_win_position(int x) {
+	win = 0;
+	near_win = 0;
+	if (x >= 268 and x <= 275) {
+		win = 1;
+	}
+	else if (x >= 238 and x <= 300 and !win) {
+			near_win = 1;
+		}
+	}
+
+void view_setup(void) {
+	/*
+	This function sets up the view volume.
+	*/
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (view_state == 0)
+	{
+		gluPerspective(159.4, canvas_Width / canvas_Height, 1.0, 150.0);
+	}
+	// Sets view volume to orthographic if view state is 1. 
+	else {
+		glOrtho(-275, 275, -275, 275, 1, 150);
+	}
+	// Sets the current matrix mode to modelview to apply the subsequent operations on modelview mode.
+	gluLookAt(eyeX, eyeY, 1, centerX, centerY, -50.0, 0.0, 1.0, 0.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
 // Timer function.
 void animate(int value) {
 	switch (value) {
@@ -276,8 +257,8 @@ void animate(int value) {
 		}
 		break;
 	case 2:
-		success_count += 1;
-		if (success_count <= 3 and win) {
+		play_count += 1;
+		if (play_count <= 3 and win) {
 			score_val += 25;
 		}
 		t = 0.0;
@@ -295,6 +276,17 @@ void animate(int value) {
 
 }
 
+void display_typed_velocity(unsigned char key) {
+	int key_in = int(key) - 48;
+	if (key_in >= 0 and key_in <= 9) {
+		h_string += key;
+		if (h_string.length() <= 7) {
+			h = stoi(h_string);
+			glutPostRedisplay();
+		}
+	}
+}
+
 void keyboard_handler(unsigned char key, int x, int y) {
 	switch (key)
 	{
@@ -305,25 +297,15 @@ void keyboard_handler(unsigned char key, int x, int y) {
 		break;
 	case 'T': case 't':
 		if (eyeY == 0) {
-			eyeY = -270;
-			view_setup();
-			centerX = 0;
-			centerY = 0;
+			eyeY = -275;
 			centerZ = -50;
-			eyeX = 0;
-			eyeZ = 0;
-			
+
 		}
 		else {
 			eyeY = 0;
-			view_setup();
-			centerX = 0;
-			centerY = 0;
-			eyeX = 0;
-			eyeZ = 0;
 			centerZ = -50;
 		}
-		
+
 		view_setup();
 		glutPostRedisplay();
 		break;
@@ -337,71 +319,55 @@ void keyboard_handler(unsigned char key, int x, int y) {
 	}
 	if (view_state == 1) {
 		if (x >= 15 and x <= 87 and y >= 250 and y <= 274) {
-			int key_in = int(key) - 48;
-			if (key_in >= 0 and key_in <= 9) {
-				h_string += key;
-				if (h_string.length() <= 7) {
-					h = stoi(h_string);
-					glutPostRedisplay();
-				}
-			}
+			display_typed_velocity(key);
 		}
-		
+
 	}
 	else if (view_state == 0) {
 		if (x >= 145 and x <= 161 and y >= 260 and y <= 272) {
-			int key_in = int(key) - 48;
-			if (key_in >= 0 and key_in <= 9) {
-				h_string += key;
-				if (h_string.length() <= 7) {
-					h = stoi(h_string);
-					glutPostRedisplay();
-				}
-			}
+			display_typed_velocity(key);
 		}
 	}
 }
 
+void enable_go_button() {
+	isAnimate = 1;
+	//glutPostRedisplay();
+	glutTimerFunc(animationPeriod, animate, 1);
+}
+
+void enable_velocity_type() {
+	h_string = "";
+	glutKeyboardFunc(keyboard_handler);
+}
+
 void mouse_handler(int button, int state, int x, int y) {
+	cout << x << endl;
+	cout << y << endl;
 	if (button == GLUT_LEFT_BUTTON, state == GLUT_UP)
 	{
 		if (view_state == 1) {
-			if (x >= 95 and x <= 107 and y >= 250 and y <= 272) {
-				isAnimate = 1;
-				glutPostRedisplay();
-				glutTimerFunc(animationPeriod, animate, 1);
-				
+			if (x >= 95 and x <= 119 and y >= 250 and y <= 272) {
+				enable_go_button();				
 			}
 
 			if (x >= 15 and x <= 87 and y >= 250 and y <= 274) {
-				h_string = "";
-				glutKeyboardFunc(keyboard_handler);
+				enable_velocity_type();
 			}
 		}
 		else if (view_state == 0) {
 			if (x >= 185 and x <= 195 and y >= 262 and y <= 270) {
-				isAnimate = 1;
-				glutTimerFunc(animationPeriod, animate, 1);
+				enable_go_button();
 			}
 
 			if (x >= 145 and x <= 161 and y >= 260 and y <= 272) {
-				//cout << "here" << endl;
-				h_string = "";
-				glutKeyboardFunc(keyboard_handler);
+				enable_velocity_type();
 			}
 		}
 	}
 }
 
-
-void display_func() {
-	glLoadIdentity();
-	
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 1.0, 1.0);
-
-	win = 0;
-	near_win = 0;
+void calc_gravity_based_ring_pos() {
 	y_pos = v * t - (g / 2.0)*t*t;
 	if (200 + y_pos >= -210) {
 		x_pos = h * t;
@@ -413,18 +379,25 @@ void display_func() {
 		isAnimate = 0;
 		glutTimerFunc(1000, animate, 2);
 	}
+}
+void display_func() {
+	glLoadIdentity();
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
 
-	print_message(-100, 260, -50);
+	print_game_instruction(-100, 260, -50);
 	draw_velocity_box(-130, 0, -50);
 	draw_go_box(-90, 0, -50);
 	draw_post(210, -275, -50, 10);
 	display_score(-130, 130, -50);
-	if (success_count < 3) {
-		glColor3f(1.0, 1.0, 0.0);
+	if (play_count < 3) {
+		//glColor3f(1.0, 1.0, 0.0);
+		calc_gravity_based_ring_pos();
 		draw_ring(-60 + x_pos, 200 + y_pos, -50, 20, 15);
+		check_win_position(x_pos);
 	}
 	else {
-		glColor3f(1.0, 0.0, 0.0);
+		//
 		display_game_over(-60, 200, -50);
 	}
 	if (win) {
